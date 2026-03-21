6f3e149a77f3e99bdff6447f80bff36d59a2c695
milkcandy <milkcandyxxxx@gmail.com>
2026-03-21 21:10:04 +0800

    请求头认证切换至X-API-Key

--- middleware/auth.go
+++ middleware/auth.go
@@ -10,13 +10,13 @@ import (
 
 // SecurityVerification 身份验证
 func SecurityVerification(c *gin.Context, k []string) bool {
-	authHeader := c.GetHeader("Authorization")
-	return AuthKey(k, authHeader)
+	XAPIKey := c.GetHeader("XAPIKey")
+	return AuthKey(k, XAPIKey)
 
 }
 
 // AuthKey 加密并验证是否存在
-func AuthKey(keylocal []string, Authorization string) bool {
-	Authorization_md5 := fmt.Sprintf("%x", md5.Sum([]byte(Authorization)))
-	return slices.Contains(keylocal, Authorization_md5)
+func AuthKey(keylocal []string, XAPIKey string) bool {
+	XAPIKey_md5 := fmt.Sprintf("%x", md5.Sum([]byte(XAPIKey)))
+	return slices.Contains(keylocal, XAPIKey_md5)
 }
