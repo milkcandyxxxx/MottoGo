67290de43902392e5f93088f909815bac1004c90
milkcandy <milkcandyxxxx@gmail.com>
2026-03-22 13:27:50 +0800

    优化排版

--- middleware/limit.go
+++ middleware/limit.go
@@ -13,9 +13,7 @@ type Ratelimit struct {
 
 // Limit 限制每秒最多访问10次
 func (r *Ratelimit) Limit(ip string) bool {
-
 	l, _ := r.limiters.LoadOrStore(ip, rate.NewLimiter(rate.Limit(10), 10))
-
 	limiter := l.(*rate.Limiter)
 	return limiter.Allow()
 }
